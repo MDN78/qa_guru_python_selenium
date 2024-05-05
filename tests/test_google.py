9cde7a82cb75cd1937f93aa0396d642a3efbd8d1
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-05-05 20:50:54 +0300

    add first steps

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, timeout=2)
 driver.get('https://www.google.com/ncr')
 # driver.find_element(By.CSS_SELECTOR, '[name=q]').send_keys('selene')
 # driver.find_element(By.CSS_SELECTOR, '[name=q]').send_keys(Keys.ENTER)
 # тождественно:
 driver.find_element(By.CSS_SELECTOR, '[name=q]').send_keys('selene', Keys.ENTER)
-
+wait.until()
 
 
 time.sleep(3)
